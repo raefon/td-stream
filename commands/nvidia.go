e8353c82caa1eb1835c690150d37d584b7e3be58
raefon <raefon@users.noreply.github.com>
2024-06-10 20:01:31 -0700

    update

--- commands/nvidia.go
+++ commands/nvidia.go
@@ -32,7 +32,7 @@ func init() {
 
 func nvidiaInstall(cmd *cobra.Command, server string) error {
 
-	startScriptCommand := "git clone https://github.com/Scotchman0/NVIDIA_Drivers/ && cd NVIDIA_Drivers && sudo ./NVIDIA_drivers.sh && echo 'Complete.... Rebooting.' && sudo reboot"
+	startScriptCommand := "sudo add-apt-repository ppa:graphics-drivers/ppa -y && sudo apt update && sudo apt install nvidia-driver-550 -y && echo 'Complete.... Rebooting.' && sudo reboot"
 
 	// Set the command to be executed over SSH
 	cmd.Flags().Set("command", startScriptCommand)
---
+++ commands/setup.go
@@ -0,0 +1,64 @@
+package commands
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	setupCmd = &cobra.Command{
+		Use:   "setup server_id",
+		Short: "Setup server for use with wolf",
+		Args:  cobra.ExactArgs(1), // Expects exactly one argument: server_id
+		RunE: func(cmd *cobra.Command, args []string) error {
+			serverID := args[0]
+
+			cmd.Flags().String("bin", "ssh", "Name of SSH client executable (e.g., ssh, mosh)")
+			cmd.Flags().String("user", "user", "User account to use for login")
+			cmd.Flags().String("command", "", "Command to execute over SSH")
+
+			return setupServerCmd(cmd, serverID)
+		},
+	}
+)
+
+func init() {
+	rootCmd.AddCommand(setupCmd)
+}
+
+func setupServerCmd(cmd *cobra.Command, server string) error {
+	err := getSetupFiles(cmd, server)
+	if err != nil {
+		return fmt.Errorf("error getting setup files: %w", err)
+	}
+
+	startScriptCommand := "bash /home/user/setup.sh"
+
+	// Set the command to be executed over SSH
+	cmd.Flags().Set("command", startScriptCommand)
+
+	// Execute the SSH command to run the script
+	return sshServer(cmd, []string{server})
+}
+
+func getSetupFiles(cmd *cobra.Command, server string) error {
+	// Define the files and their target location
+	files := []string{"setup.sh"}
+	targetDir := "/home/user/"
+
+	// Build the curl command to fetch files from GitHub and place them in the target directory
+	curlCommands := make([]string, len(files))
+	for i, file := range files {
+		url := fmt.Sprintf("https://raw.githubusercontent.com/raefon/td-stream/main/setup/%s", file)
+		curlCommands[i] = fmt.Sprintf("curl -o %s%s %s", targetDir, file, url)
+	}
+	fullCurlCommand := strings.Join(curlCommands, " && ")
+
+	// Set the command to be executed over SSH
+	cmd.Flags().Set("command", fullCurlCommand)
+
+	// Execute the SSH command
+	return sshServer(cmd, []string{server})
+}
